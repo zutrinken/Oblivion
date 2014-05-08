e3ef87b5e19498818f253eba928be53e7d0845d0
zutrinken <xoxo@peteramende.de>
2014-05-08 22:10:35 +0200

    added fade in for posts on frontpage

--- js/front-page.js
+++ js/front-page.js
@@ -6,3 +6,28 @@ var msnry = new Masonry( container, {
 	columnWidth				: 1,
 	transitionDuration		: 0
 });
+
+jQuery(function($) {
+	var viewport = $(window);
+	function fading() {
+		$('.featured-post').each( function(){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = viewport.scrollTop() + viewport.height();
+
+			if( bottom_of_window > bottom_of_object ) {
+				$(this).addClass('show');
+			} else {
+/* 				$(this).removeClass('show'); */
+			}
+		});
+	}
+	if($('html').hasClass('no-touch')) {
+		fading();
+		viewport.scroll(fading);
+		viewport.resize(fading);
+		viewport.bind('orientationchange', function() {
+			fading();
+		});
+	}
+});
