a70d14e4da740251905df2e30dffd535572859fe
zutrinken <xoxo@peteramende.de>
2014-06-28 00:36:09 +0200

    changed some scroll fx on front-page

--- js/front-page.js
+++ js/front-page.js
@@ -12,22 +12,28 @@ jQuery(function($) {
 	function fading() {
 		$('.featured-post').each( function(){
 
-			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_object = $(this).offset().top + 128;
 			var bottom_of_window = viewport.scrollTop() + viewport.height();
 
 			if( bottom_of_window > bottom_of_object ) {
 				$(this).addClass('show');
 			} else {
-/* 				$(this).removeClass('show'); */
+				$(this).removeClass('show');
 			}
 		});
 	}
 	if($('html').hasClass('no-touch')) {
 		fading();
-		viewport.scroll(fading);
-		viewport.resize(fading);
-		viewport.bind('orientationchange', function() {
-			fading();
+		viewport.on({
+			'scroll': function() {
+				fading();
+			},
+			'resize': function() {
+				fading();
+			},
+			'orientationchange': function() {
+				fading();
+			}
 		});
 	}
 });
