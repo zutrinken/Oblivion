a43503fbefd640e6df2f3de3d145a9f57f84eba0
zutrinken <xoxo@peteramende.de>
2014-09-10 21:26:36 +0200

    add theme support automatic feed links

--- functions.php
+++ functions.php
@@ -82,6 +82,10 @@ add_theme_support (
 	)
 );
 
+/* Enable post and comment RSS feed links to head */
+add_theme_support ('automatic-feed-links');
+
+
 	/* ==========================================================================
 	   WP Title
 	   ========================================================================== */
