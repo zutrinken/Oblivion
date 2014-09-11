d7a7814ce11dc3990efe879e5ca109bb872b55a4
zutrinken <xoxo@peteramende.de>
2014-09-11 16:29:39 +0200

    update comment tepmlate

--- comments.php
+++ comments.php
@@ -1,12 +1,7 @@
- <?php
-	_deprecated_file( sprintf( __('Theme without %1$s','oblivion'), basename(__FILE__) ), '3.0', null, sprintf( __('Please include a %1$s template in your theme.','oblivion'), basename(__FILE__) ) );
-	/* Do not delete these lines */
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
-	if ( post_password_required() ) {
-		?><p class="nocomments"><?php _e('This is not the content you are looking for.','oblivion'); ?></p><?php
-		return;
-	}
+<?php
+if ( post_password_required() ) {
+	return;
+}
 ?>
 
 <?php if (have_comments()) : ?>
@@ -32,51 +27,38 @@
 <?php if(comments_open()) : ?>
 	<div id="respond-wrapper">
 		<div class="inner">
-			<h3 id="reply"><?php comment_form_title( __('What do you think?','oblivion'), '<i class="fa fa-reply"></i> ' . __('Leave a reply to %s','oblivion') ); ?></h3>
-			<?php cancel_comment_reply_link('<i class="fa fa-times"></i> <span>'. __('cancel reply','oblivion').'</span>') ?>
-			<div class="clear"></div>
-			<div id="respond">
-				<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-					<p>
-						<?php printf(__('You have to be <a href="%s">logged in</a>.','oblivion'), wp_login_url( get_permalink() )); ?>
-					</p>
-				<?php else : ?>
-					<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-						<?php if(is_user_logged_in()) : ?>
-							<p>
-								<small><?php printf(__('Logged in as <a href="%1$s">%2$s</a>.','oblivion'), get_option('siteurl') . '/wp-admin/profile.php', $user_identity); ?> <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="<?php _e('Log out of this account','oblivion'); ?>"><?php _e('Log out','oblivion'); ?> &raquo;</a></small>
-							</p>
-						<?php else : ?>
-							<p class="input-col input-col-1">
-								<label for="author"><?php _e('Name','oblivion'); ?> <?php if ($req) { ?><span class="req"><?php _e('required','oblivion'); ?></span><?php } ?></label>
-								<input class="input" type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-	
-							</p>
-				
-							<p class="input-col input-col-2">
-								<label for="email"><?php _e('Email','oblivion'); ?> <?php if ($req) { ?><span class="req"><?php _e('required','oblivion'); ?></span><?php } ?></label>
-								<input class="input" type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-	
-							</p>
-				
-							<p class="input-col input-col-3">
-								<label for="url"><?php _e('Website','oblivion'); ?></label>
-								<input class="input" type="text" name="url" id="url" value="<?php echo  esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
-							</p>
-							<div class="clear"></div>
-						<?php endif; ?>
-						<p class="input-col input-col-4">
-							<label for="comment"><?php _e('Comment','oblivion'); ?> <?php if ($req) { ?><span class="req"><?php _e('required','oblivion'); ?></span><?php } ?></label>
-							<textarea class="input" name="comment" id="comment" cols="58" rows="10" tabindex="5"></textarea>
-						</p>
-						<p class="input-col input-col-5">
-							<input class="input" name="submit" type="submit" id="submit" class="button" tabindex="6" value="<?php _e('Submit','oblivion'); ?>" />
-							<?php comment_id_fields(); ?>
-						</p>
-						<?php do_action('comment_form', $post->ID); ?>
-					</form>
-				<?php endif; ?>
-			</div>
+			<?php
+			$req = get_option( 'require_name_email' );
+			$aria_req = ( $req ? " aria-required='true'" : '' );
+			$args = array(
+				'id_form'           => 'commentform',
+				'id_submit'         => 'submit',
+				'cancel_reply_link' => '<i class="fa fa-times"></i> <span>' . __('Cancel Reply','oblivion') . '</span>',
+				'label_submit'      => __('Post Comment','oblivion'),
+				'comment_notes_after' => '',
+				'fields' => apply_filters( 'comment_form_default_fields', array(
+					'author' =>
+					'<p class="comment-form-author">' .
+					'<label for="author">' . __('Name','oblivion') . '</label> ' .
+					( $req ? '<span class="required">*</span>' : '' ) .
+					'<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+					'" size="30"' . $aria_req . ' /></p>',
+					
+					'email' =>
+					'<p class="comment-form-email"><label for="email">' . __('Email','oblivion') . '</label> ' .
+					( $req ? '<span class="required">*</span>' : '' ) .
+					'<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+					'" size="30"' . $aria_req . ' /></p>',
+					
+					'url' =>
+					'<p class="comment-form-url"><label for="url">' .
+					__('Website','oblivion') . '</label>' .
+					'<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+					'" size="30" /></p>'
+					)
+				)
+			); ?>
+			<?php comment_form($args); ?>
 		</div>
 	</div>
 <?php endif; ?>
