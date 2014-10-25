5ea9d8436294184255b4da41a610a4fae149d85e
zutrinken <xoxo@peteramende.de>
2014-10-25 22:58:00 +0200

    changed 404

--- 404.php
+++ 404.php
@@ -11,13 +11,5 @@
 			</div>
 			<div class="page-header-cover" data-type="prlx" data-speed="0.625"></div>
 		</section>
-		<section id="post-404" class="page hentry">
-			<div class="inner">
-				<article class="post-content post-article">
-					<p><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/penguin.png" width="256" height="256" style="padding-bottom: 4.8em;" /></a></p>
-				</article>
-				<div class="clear"></div>
-			</div>
-		</section>
 
 <?php get_footer(); ?>
